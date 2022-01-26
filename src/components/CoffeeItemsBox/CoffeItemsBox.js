da9ddaeecb71e91f361f8583f218e28cadb02cc4
github-usernameandrej-a <amelyanovich.andrej@mail.ru>
2022-01-26 14:29:48 +0300

    feat: add react-transition

--- src/components/CoffeeItemsBox/CoffeItemsBox.js
+++ src/components/CoffeeItemsBox/CoffeItemsBox.js
@@ -1,12 +1,13 @@
 import "./CoffeeItemsBox.scss";
 import image from "../../resources/ourCoffeeCards.png";
 import { useEffect, useState } from "react";
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { Link } from "react-router-dom";
 import _doShortName from "../../service/service";
 
 const CoffeItemsBox = (props) => {
-    const [state, setData] = useState(null);
-
+    const [state, setData] = useState([]);
+    const [show, setShow] = useState(false);
     useEffect(() => {
         onSetData(props.data);
     }, [props.data]);
@@ -15,12 +16,28 @@ const CoffeItemsBox = (props) => {
     const onSetData = (array) => {
         _doShortName(array);
         setData(array);
+        setShow(true);
     };
 
-    const printItems = (array) => {
-        const items = array.map(item => {
-            return(
-                <Link key={item.id} to={`/ourcoffee/${item.id}`}>
+    return(
+         <>
+            <PrintItems show={show} array={state}/>
+         </>
+    )
+}
+
+const PrintItems = (props) => {
+    const timeout = 1000;
+    const items = props.array.map(item => {
+        return(
+
+            <CSSTransition
+            timeout={timeout}
+            classNames="cards"
+            in={props.show}
+            key={item.id}
+            >
+                <Link className="cards" to={`/ourcoffee/${item.id}`}>
                     <div className="coffee_items_item">
                         <div className="coffee_items_item_image">
                             <img src={image} alt="" />
@@ -30,23 +47,17 @@ const CoffeItemsBox = (props) => {
                         <div className="coffee_items_item_price text"> {item.price}$ </div>
                     </div>
                 </Link>
-            )
-        });
-
-        return(
-            <div className="coffee_items">
-                {items}
-            </div>
+            </CSSTransition>
         )
-    };
-
-    const content = state ? printItems(state) : null;
+    });
 
     return(
-         <>
-            {content}
-         </>
+        <div className="coffee_items">
+            <TransitionGroup component={null}>
+                {items}
+            </TransitionGroup>
+        </div>
     )
-}
+};
 
 export default CoffeItemsBox;
--- src/components/Main/OurBest/OurBest.js
+++ src/components/Main/OurBest/OurBest.js
@@ -1,13 +1,15 @@
 import "./OurBest.scss";
 import firstCard from "../../../resources/cards__image/1.png";
 
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import data from "../../../data/data";
 import _doShortName from "../../../service/service"
-const OurBest = (props) => {
-    const [state, setState] = useState(null);
 
+const OurBest = (props) => {
+const [state, setState] = useState([]);
+const [show, setShow] = useState(false);
     useEffect(() => {
         onState(data);
     }, [data]);
@@ -17,36 +19,12 @@ const OurBest = (props) => {
             const bestItems = array.filter(item => item["best"]);
             _doShortName(bestItems); 
             setState(bestItems);
+            setShow(true);
         } catch(e){
             console.log( `Error: ${e.name}: ${e.message}`);
         }
     };
 
-    const onSetCards = (array) => {
-        const items = array.map(item => {
-            return(
-                     <Link key={item.id} to={`/ourcoffee/${item.id}`}>
-                        <li className="best__list__items__item">
-                            <img src={firstCard} alt="first" />
-                            <div className="title">
-                                <p className="text">{item.name} {item.weight} kg</p>
-                            </div>
-                            <div className="cost">
-                                <p>{item.price}$</p>
-                            </div>
-                        </li>
-                     </Link>
-            )
-        });
-
-        return(
-            <ul className="best__list__items">
-                  {items} 
-            </ul>
-        )
-    };
-
-    const content = state ? onSetCards(state) : null;
     return(
         <div className="best">
             <div className="best__title subheader_text">
@@ -54,9 +32,43 @@ const OurBest = (props) => {
             </div>
 
             <div className="best__list">
-                {content}
+                <SetCards show={show} array={state}/>
             </div>
         </div> /* \best */
+    )
+};
+
+const SetCards = (props) => {
+    const time = 1000;
+    const items = props.array.map(item => {
+        return(
+            <CSSTransition
+            timeout={time}
+            classNames="cards"
+            in={props.show}
+            key={item.id}
+            >
+                <Link className="cards" to={`/ourcoffee/${item.id}`}>
+                    <li className="best__list__items__item">
+                        <img src={firstCard} alt="first" />
+                        <div className="title">
+                            <p className="text">{item.name} {item.weight} kg</p>
+                        </div>
+                        <div className="cost">
+                            <p>{item.price}$</p>
+                        </div>
+                    </li>
+                 </Link>
+            </CSSTransition>
+        )
+    });
+
+    return(
+        <ul className="best__list__items">
+            <TransitionGroup component={null}>
+                {items}      
+            </TransitionGroup> 
+        </ul>
     )
 };
 
